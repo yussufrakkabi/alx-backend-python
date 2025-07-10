d3f082314d822ef1ba8cea5e64a837076f123636
yussufrakkabi <yussufrakkabi@outlook.com>
2025-07-10 16:57:04 +0100

    3retry..cachquery..

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,37 @@
+import sqlite3
+import functools
+
+query_cache = {}
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            return func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+    return wrapper
+
+def cache_query(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        query = kwargs.get("query") or (args[1] if len(args) > 1 else None)
+        if query in query_cache:
+            print("✅ Using cached result.")
+            return query_cache[query]
+        result = func(conn, *args, **kwargs)
+        query_cache[query] = result
+        return result
+    return wrapper
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+users = fetch_users_with_cache(query="SELECT * FROM users")
+users_again = fetch_users_with_cache(query="SELECT * FROM users")
